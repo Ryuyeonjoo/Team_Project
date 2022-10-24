dfb87bc8b9eae1d936d1e6841b93d973a79b55ec
bhs1212 <hasun17kh@gmail.com>
2022-10-24 23:07:52 +0900

    hasun

--- Team_Project/src/Survey.java
+++ Team_Project/src/Survey.java
@@ -7,7 +7,7 @@ public class Survey {
     public void SurveyFunction(Statement statement){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
+        System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
 
         System.out.print("성함) ");
         String name = sc.nextLine();
