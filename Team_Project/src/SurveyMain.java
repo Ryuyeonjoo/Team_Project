60fb7f5983510a3046d2c9c87ef027bf50e4d1c7
mayds1010 <mayds1010@gmail.com>
2022-10-24 22:16:09 +0900

    commit

--- Team_Project/src/SurveyMain.java
+++ Team_Project/src/SurveyMain.java
@@ -17,13 +17,16 @@ public class SurveyMain {
 
             System.out.print("P.설문시작 | S.통계 | Q.종료");
                String flag = sc.next();
-            while(!flag.equals('Q')){             
+            
+               while(!flag.equals('Q')){             
                 if(flag.equals('P') ){
                     SurveyQuestion question = new SurveyQuestion();
                     question.QuestionFunction(statement);
+               
                 } else if(flag.equals('S')){
                     SurveyTotal total = new SurveyTotal();
                     total.TotalFunction(statement);
+                
                 } else{
                     System.out.print("소중한 시간을 내어 설문에 참여해 주셔서 감사합니다." +
                     "앞으로도 고객님께서 더욱 만족하실 수 있도록 노력하는 KHCAFE가 되겠습니다." +
