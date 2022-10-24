3f62c9b40c49f5d95ec6cd3ff0ab117b1bbfa579
bhs1212 <hasun17kh@gmail.com>
2022-10-24 20:05:19 +0900

    commit

--- Survey/SurveySelect.java
+++ Survey/SurveySelect.java
@@ -1,9 +1,25 @@
 package Survey;
 
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class SurveySelect {
     public void SelectFunction(Statement statement){
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("");
+        String query = "SELECT * FROM users_list ";
+        ResultSet resultSet;
+        try{
+            resultSet = statement.executeQuery(query);
+
+            while(resultSet.next()){
+                String USER_UID = resultSet.getString("USERS_UID");
+                System.out.println("성함 : " + USER_UID);
+                System.out.println("휴대폰 번호 : " + resultSet.getString("PHONE"));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
