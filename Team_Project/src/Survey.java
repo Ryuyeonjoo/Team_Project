c61045479d89c9a168b562d72b5f7f0d289eb1e0
Ryuyeonjoo <fuswn87@gmail.com>
2022-10-26 01:56:16 +0900

    yeonjoo commit

--- Team_Project/src/Survey.java
+++ Team_Project/src/Survey.java
@@ -4,61 +4,76 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 
+
+
 public class Survey {
     public void SurveyFunction(Statement statement, Statement statement2){
         Scanner sc = new Scanner(System.in);
 
-        String phonenumber = ""; //번호가 8자리가 아니면 while밑부터 다시 하고 싶은데 메인으로 돌아가 처음부터 다시 실행됨
- 
-        while (true) {
-        System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해 주세요.");
-        System.out.print("성함 ) ");
-       
-        Scanner scanner = new Scanner(System.in);
-        String aaa = sc.next();
-       
-                System.out.print("휴대폰 번호 8자리 )");
-                int num = sc.nextInt();
-       
-               if((int)(Math.log10(num)+1) == 8) {
-               System.out.print("설문을 시작합니다.");
-             
-            }else{
-                System.out.println("다시 입력 바랍니다.");
-                return;
-            }
+        System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
 
+        while(true) {
+            System.out.print("성함) ");
+            String name = sc.nextLine();
+            if (!name.matches(".*[0-9].*")) {
+               break;
+            }
+            System.out.println("문자만 입력해주세요");
+         }
+         while(true) {
+            System.out.print("휴대폰 번호 (8자리)) ");
+            String phone = sc.nextLine();
+            if (phone.matches("([0-9]{4})-?([0-9]{4})")) {
+               break;
+            }
+            System.out.println("8자리를 입력해주세요");
+         }
+        
         System.out.println("------------------------------------------------------");
-        System.out.println("고객님께서 방문하신 KHCAFE 강남점에 관련한 설문 조사입니다.\n"+
+        System.out.println("고객님께서 방문하신 CAFE에 관련한 설문 조사입니다.\n"+
         "1번에서부터 5번까지 고객님께서 매장에 대해 경험하신 내용을 바탕으로\n" +
         "설문에 임해주시기 바랍니다.");
 
         String question = "SELECT * FROM questions_list ORDER BY QUESTIONS";
         ResultSet resultSet;
-        String example = "SELECT * FROM example_list";
+        String example = "SELECT * FROM example_list ORDER BY EXAMPLE";
         ResultSet resultSet2;
+       
         
         try{
+            
+            
             resultSet = statement.executeQuery(question);
             //문항 출력
-            while(resultSet.next()){
-                resultSet2 = statement2.executeQuery(example);
-
-                System.out.println(resultSet.getString("QUESTIONS"));         
-                while(resultSet2.next()){
-                    System.out.print(resultSet2.getString("EXAMPLE"));
+            while(resultSet.next()){               
+            resultSet2 = statement2.executeQuery(example);
+                System.out.println(resultSet.getString("QUESTIONS"));   
+                    while(resultSet2.next()){
+                    System.out.print(resultSet2.getString("EXAMPLE"));    
                     System.out.print(" ");
+                    
                 }
                 
                 System.out.println("");
                 System.out.print("답변 : ");
-                int answer = sc.nextInt();
+
+                int answer = 0;
+                while (true){
+                    answer = sc.nextInt();
+                   if(answer <= 5 && answer >= 1) {
+                      break;
+                   } 
+                   System.out.println( "1부터 5까지의 답만 입력해주세요.");
+                   }
             }
         } catch(SQLException e){
             e.printStackTrace();
         }
     }
-}
+    public void example(Statement statement){
+        
+    }
+    
 }
 
 
