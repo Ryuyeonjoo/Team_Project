87ae35cc86e8f7e16917cf9a61e655543662ef92
bhs1212 <hasun17kh@gmail.com>
2022-10-25 22:58:58 +0900

    commit

--- Team_Project/src/Survey.java
+++ Team_Project/src/Survey.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 
 public class Survey {
-    public void SurveyFunction(Statement statement, Statement statement2){
+    public void SurveyFunction(Statement statement){
         Scanner sc = new Scanner(System.in);
 
         System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
@@ -21,23 +21,20 @@ public class Survey {
         "1번에서부터 5번까지 고객님께서 매장에 대해 경험하신 내용을 바탕으로\n" +
         "설문에 임해주시기 바랍니다.");
 
-        String question = "SELECT * FROM questions_list ORDER BY QUESTIONS";
+        String query = "SELECT answers.QUESTIONS_UID, answers.EXAMPLE_UID"
+        + "FROM (answers inner JOIN questions_list ON answers.QUESTIONS_UID = questions_list.QUESTIONS_UID)"
+        + "INNER JOIN example_list ON answers.EXAMPLE_UID = example_list.EXAMPLE_UID";
         ResultSet resultSet;
-        String example = "SELECT * FROM example_list";
-        ResultSet resultSet2;
+
         
         try{
-            resultSet = statement.executeQuery(question);
-            resultSet2 = statement2.executeQuery(example);
+            resultSet = statement.executeQuery(query);
+
             //문항 출력
             while(resultSet.next()){
 
-                System.out.println(resultSet.getString("QUESTIONS"));         
-                while(resultSet2.next()){
-                    System.out.print(resultSet2.getString("EXAMPLE"));
-                    System.out.print(" ");
-                }
-                
+                System.out.println(resultSet.getString("QUESTIONS_UID"));         
+             
                 System.out.println("");
                 System.out.print("답변 : ");
                 int answer = sc.nextInt();
@@ -46,9 +43,5 @@ public class Survey {
             e.printStackTrace();
         }
     }
-    public void example(Statement statement){
-        
-    }
-    
 }
  
