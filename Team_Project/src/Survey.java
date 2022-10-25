a1927f8544921553e2aa64de6f1a08cee1343288
mayds1010 <mayds1010@gmail.com>
2022-10-26 00:59:42 +0900

    survey

--- Team_Project/src/Survey.java
+++ Team_Project/src/Survey.java
@@ -8,12 +8,25 @@ public class Survey {
     public void SurveyFunction(Statement statement, Statement statement2){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
+        String phonnumber = ""; //번호가 8자리가 아니면 while밑부터 다시 하고 싶은데 메인으로 돌아가 처음부터 다시 실행됨 
 
-        System.out.print("성함) ");
-        String name = sc.nextLine();
-        System.out.print("휴대폰 번호 8자리) ");
-        String phone = sc.nextLine();
+        while (true) {
+        System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해 주세요.");
+        System.out.print("성함 ) ");
+        
+        Scanner scanner = new Scanner(System.in);
+        String aaa = sc.next();
+        
+                System.out.print("휴대폰 번호 8자리 )");
+                int num = sc.nextInt();
+        
+               if((int)(Math.log10(num)+1) == 8) {
+               System.out.print("설문을 시작합니다.");
+             
+            }else{
+                System.out.println("다시 입력 바랍니다.");
+                return;
+            }
         System.out.println("------------------------------------------------------");
         System.out.println("고객님께서 방문하신 CAFE에 관련한 설문 조사입니다.\n"+
         "1번에서부터 5번까지 고객님께서 매장에 대해 경험하신 내용을 바탕으로\n" +
@@ -49,8 +62,22 @@ public class Survey {
             e.printStackTrace();
         }
     }
+    }
     public void example(Statement statement){
         
     }
     
 }   
+
+
+
+
+
+
+// System.out.println("고객님의 성함과 010을 제외한 휴대폰 번호 뒷자리를 입력해주세요.");
+
+//         System.out.print("성함) ");
+//         String name = sc.nextLine();
+//         System.out.print("휴대폰 번호 8자리) ");
+//         String phone = sc.nextLine();
+//         System.out.println("------------------------------------------------------");
