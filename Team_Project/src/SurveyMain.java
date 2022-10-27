4afb44fd8212f20c027f57d66a938c2fe2602346
bhs1212 <hasun17kh@gmail.com>
2022-10-27 09:31:51 +0900

    commit

--- Team_Project/src/SurveyMain.java
+++ Team_Project/src/SurveyMain.java
@@ -5,7 +5,7 @@ public class SurveyMain {
         public static void main(String[] args){
 
             Scanner sc = new Scanner(System.in);
-            // Done - MySQL workbench  실행 : JDBC - 
+            // Done - MySQL workbench  실행 : JDBC 
             // - User/password와 접속 IP:port(localhost:3306) 접속
             String url = "jdbc:mysql://localhost:3306/survey";
             String user = "root";
@@ -13,22 +13,24 @@ public class SurveyMain {
             // - database 지정
             try{
                 Connection connection = DriverManager.getConnection(url, user, password);
+                //query Edit
                 Statement statement = connection.createStatement();
                 Statement statement2 = connection.createStatement();
                 
     
                 while(true){             
-                    System.out.println("P.설문시작 | S.통계 | Q.종료");
+                    System.out.println("P.설문시작 | S.통계 | Q.종료"); //설문시작,통계,종료
                     String choice = sc.nextLine();
+                    
                     if(choice.equals("P") ){
                         Survey question = new Survey();
                         question.SurveyFunction(statement,statement2);
                     } else if(choice.equals("S")){
                         SurveyTotal total = new SurveyTotal();
-                        total.TotalFunction(statement);
+                        total.TotalFunction(connection);
                     } else if(choice.equals("Q")){
                         System.out.print("소중한 시간을 내어 설문에 참여해 주셔서 감사합니다." +
-                        "앞으로도 고객님께서 더욱 만족하실 수 있도록 노력하는 CAFE가 되겠습니다." +
+                        "앞으로도 고객님께서 더욱 만족하실 수 있도록 노력하는 KHCAFE가 되겠습니다." +
                         "감사합니다.");
                         break;
                     } else {
--- Team_Project/src/SurveyTotal.java
+++ Team_Project/src/SurveyTotal.java
@@ -1,7 +1,58 @@
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class SurveyTotal {
-    public void TotalFunction(Statement statement){
-
+    public void TotalFunction(Connection connection){
+        Scanner sc = new Scanner(System.in);
+        Statement statement3;
+        ResultSet userRs;
+        Statement statement2;
+        ResultSet elRs;
+        try{                
+            statement2 = connection.createStatement();
+            statement3 = connection.createStatement();
+            while(true){             
+                System.out.println("1. 설문자별 답변 결과  | 2. 질문별 총 답변수 ");
+                String choice = sc.nextLine();
+                if(choice.equals("1") ) {
+                    System.out.println("선택) 1. 설문자별 답변 결과\n이름\t질문(1)\t질문(2)\t질문(3)\t질문(4)\t질문(5)");
+                    String userQuery = "SELECT * FROM USERS_LIST";
+                    userRs = statement3.executeQuery(userQuery);
+                    while(userRs.next()){
+                        String temp = userRs.getString(1);
+                        temp = temp.replaceAll("[^\\d]", "");
+                        int user_id = Integer.parseInt(temp);
+                        String name = userRs.getString(3);
+                        String pr = name + "\t\t";
+                            String strQuery = "SELECT EXAMPLE_UID FROM survey where USERS_UID = 'U"+user_id+"'";
+                            elRs = statement2.executeQuery(strQuery);
+                            while(elRs.next()){
+                                String elTemp = elRs.getString(1);
+                                elTemp = elTemp.replaceAll("[^\\d]", "");
+                                int el = Integer.parseInt(elTemp);
+                                pr = pr + el + "\t";
+                            }            
+                        System.out.println(pr);
+                    }
+                } else if(choice.equals("2")){
+                    System.out.println("선택) 2. 질문별 총 답변수\n구분\t전혀 아니다\t아니다\t\t보통이다\t그렇다\t\t매우 그렇다\t");
+                    for (int i = 1 ; i <= 5; i++ ) {
+                        String pr = "질문("+i+")\t\t";
+                        for (int j = 1; j <= 5; j++) {
+                            String q = "SELECT COUNT(QUESTIONS_UID) FROM survey where EXAMPLE_UID = 'E"+j+"'AND QUESTIONS_UID = 'Q"+i+"'";
+                            ResultSet rs = statement3.executeQuery(q);
+                            if(rs.next()){
+                                pr = pr + rs.getInt(1) + "\t\t";
+                            } 
+                        }
+                        System.out.println(pr);
+                    }
+                }
+            }
+            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        sc.close();
     }
 }
